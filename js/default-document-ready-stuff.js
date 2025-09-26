2c15f65490151b4dc9ed92716b782578d7232b27
ww24e018 <ww24e018@technikum-wien.at>
2025-09-26 10:06:21 +0200

    modular standard import pattern

---
+++ js/default-document-ready-stuff.js
@@ -0,0 +1,9 @@
+import load_navbar from "./navbar-loading.js";
+
+// assume jquery loaded at this point
+// also: the ready-handler-add here implies "do a side-effect only import in the html file
+// like: import '../js/default-document-ready-stuff
+$(document).ready(function() {
+    console.log('default document ready stuff initialized');
+    load_navbar();
+});
---
+++ js/navbar-loading.js
@@ -0,0 +1,3 @@
+export default function load_navbar() {
+    $('#insert-navbar-here').load('../htmls/000-navbar.html');
+}
