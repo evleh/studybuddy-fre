7c45864f698f5c3baf667c78acc1c2f0e420e42f
evleh <100370471+evleh@users.noreply.github.com>
2025-11-08 11:16:06 +0100

    Feature/FRE-28_create-new-box-functionality (#29)
    post-request, input validation, adapted responsitivity to tilted phone, enabled custom feedback when form is invalid, unsuccessful http-reponse shows error message, user is greeted by name

---
+++ js/010-new-box.js
@@ -0,0 +1,70 @@
+import constants from "./constants.js";
+
+
+$(document).ready(function (){
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (() => {
+            'use strict'
+
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            const forms = document.querySelectorAll('.needs-validation')
+
+            // Loop over them and prevent submission
+            Array.from(forms).forEach(form => {
+                form.addEventListener('submit', event => {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+        })()
+
+        // POST Request
+        $("#new-box").on("submit", function (e){
+            e.preventDefault();
+
+            var title = $("#box-title").val();
+            var description = $("#box-description").val();
+            var isPublic = $("#is-public-box").is(":checked");
+            var urlRedirect = "../htmls/011-edit-box.html";
+
+            const post_data = {title: title, description: description, isPublic:isPublic}
+            console.log(post_data)
+            $.ajax({
+                url: constants.BOXES_URL,
+                type: "POST",
+                contentType: "application/json",
+                data: JSON.stringify(post_data),
+                timeout: 3000, // nach 3 Sekunden abbrechen
+                headers: { Authorization: `Bearer ${sessionStorage.getItem('accessToken')}` },
+                success: function (data){
+                    console.log("Post gespeichert: ", data);
+                    window.location.href = urlRedirect;
+                    console.log("data gespeichert ")
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.error("Fehler beim Speichern:", textStatus, errorThrown);
+
+                    const msg = jqXHR.responseJSON?.message || errorThrown || "Unbekannter Fehler";
+
+                    if (window.sb?.appendNotification) {
+                        window.sb.appendNotification({
+                            title: 'Registrierung nicht erfolgreich',
+                            type: window.sb.alertTypes.ERROR,
+                            message: msg,
+                            scrollIntoView: false
+                        });
+                    } else {
+                        alert("Fehler: " + msg); // Fallback
+                    }
+                }
+            })
+        })
+
+    }
+
+)
+
