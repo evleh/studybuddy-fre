ffb9cbf7f92823cd26c85a3e148ba1fdb3d9a794
soschoen <sophie.schoenwaelder.rieder@gmail.com>
2026-02-18 12:05:13 +0100

    made home base show own boxes from backend. Buttons not working yet.

--- js/004-home-base.js
+++ js/004-home-base.js
@@ -1,16 +1,26 @@
 import constants from "./constants.js";
+import {get_me_ownboxes} from "./bae-connect-me.js";
 
-$(document).ready(function (){
-    $.ajax({
-        method: "GET",
-        url: "../DummyData/boxes.json",
-        dataType: "json"
-    }).done(function (data){
-        renderMyBoxes(data);
 
-    })
+$(document).ready(async function () {
+    let own_boxes = await get_me_ownboxes()
 
-})
+    //let boxtitles = own_boxes.map((box) => box.title);
+    //boxtitles.sort()
+
+
+    $.each(own_boxes, function (index, item) {
+
+        $(document.getElementById("list-of-own-boxes")).append('<li class="list-group-item">'
+            + "<p>" + "<b>Titel:</b> " + "<b>" + item.title + "</b>" + "</p>"
+            + "<p>" + "<b>Beschreibung:</b> " + item.description + "</p>"
+            + "<p>" + "<b>Sichtbarkeit:</b> " + item.public + "</p>"
+            + '<button>Fragen ansehen</button>'
+            + '<button>Bearbeiten</button>'
+            +
+        "</li>");
+    });
+});
 
 function renderMyBoxes(data) {
     $.each(data, function (i, boxData) {
