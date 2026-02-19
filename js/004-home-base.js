8f074fe3321186f5829800442870314609ae17ca
soschoen <sophie.schoenwaelder.rieder@gmail.com>
2026-02-19 14:04:55 +0100

    edit button works

--- js/004-home-base.js
+++ js/004-home-base.js
@@ -3,22 +3,31 @@ import {get_me_ownboxes} from "./bae-connect-me.js";
 
 
 $(document).ready(async function () {
-    let own_boxes = await get_me_ownboxes()
+    let own_boxes = await get_me_ownboxes();
+    let urlEdit;
 
-    //let boxtitles = own_boxes.map((box) => box.title);
-    //boxtitles.sort()
 
 
     $.each(own_boxes, function (index, item) {
+
+        urlEdit = "http://localhost:63343/studybuddy-fre/htmls/011-edit-box.html?id="+item.id;
+
+
 
         $(document.getElementById("list-of-own-boxes")).append('<li class="list-group-item">'
             + "<p>" + "<b>Titel:</b> " + "<b>" + item.title + "</b>" + "</p>"
             + "<p>" + "<b>Beschreibung:</b> " + item.description + "</p>"
             + "<p>" + "<b>Sichtbarkeit:</b> " + item.public + "</p>"
+            + "<p>" + "<b>Id:</b> " + item.id + "</p>"
             + '<button>Fragen ansehen</button>'
-            + '<button>Bearbeiten</button>'
+            + '<button id="ButtonEdit">Bearbeiten</button>'
+
             +
         "</li>");
+    });
+
+    document.getElementById('ButtonEdit').addEventListener('click', function() {
+        window.location.href = urlEdit;
     });
 });
 
