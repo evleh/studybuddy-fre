bd0763a31a2d73052c0a764aae2022214ed63ca1
soschoen <sophie.schoenwaelder.rieder@gmail.com>
2026-02-19 14:50:32 +0100

    view questions button works

--- js/004-home-base.js
+++ js/004-home-base.js
@@ -5,12 +5,18 @@ import {get_me_ownboxes} from "./bae-connect-me.js";
 $(document).ready(async function () {
     let own_boxes = await get_me_ownboxes();
     let urlEdit;
+    let urlQuestions;
 
 
 
     $.each(own_boxes, function (index, item) {
 
         urlEdit = "http://localhost:63343/studybuddy-fre/htmls/011-edit-box.html?id="+item.id;
+        //TODO: Refers to 012-new-card as a quick fix
+        // -> make new site to see questions and flip open answers in 007-view-cards!
+
+        urlQuestions = "http://localhost:63343/studybuddy-fre/htmls/012-new-card.html?id="+item.id;
+
 
 
 
@@ -18,10 +24,8 @@ $(document).ready(async function () {
             + "<p>" + "<b>Titel:</b> " + "<b>" + item.title + "</b>" + "</p>"
             + "<p>" + "<b>Beschreibung:</b> " + item.description + "</p>"
             + "<p>" + "<b>Sichtbarkeit:</b> " + item.public + "</p>"
-            + "<p>" + "<b>Id:</b> " + item.id + "</p>"
-            + '<button>Fragen ansehen</button>'
-            + '<button id="ButtonEdit">Bearbeiten</button>'
-
+            + '<button class="btn btn-primary" id="ButtonQuestions" style="margin-right: 2%">Fragen ansehen</button>'
+            + '<button class="btn btn-primary" id="ButtonEdit">Bearbeiten</button>'
             +
         "</li>");
     });
@@ -29,6 +33,14 @@ $(document).ready(async function () {
     document.getElementById('ButtonEdit').addEventListener('click', function() {
         window.location.href = urlEdit;
     });
+
+    document.getElementById('ButtonQuestions').addEventListener('click', function() {
+        window.location.href = urlQuestions;
+
+    });
+
+
+
 });
 
 function renderMyBoxes(data) {
