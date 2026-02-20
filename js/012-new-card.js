40dafccf64a9a194a3e793a24e334e30dc86474f
ww24e018 <ww24e018@technikum-wien.at>
2026-02-20 14:07:28 +0100

    Bugfix/81 known issues at card edit (#85)
    * fix all the bugs (all = tbd)
    
    * rework media deletion decision tree to include delete-old-after-setting-new
    
    * cleanup
    
    * fix mixed sentence comment

--- js/012-new-card.js
+++ js/012-new-card.js
@@ -1,6 +1,6 @@
 import {read_box} from "./bae-connect-boxes.js";
 import {change_card, create_card, delete_card, read_card} from "./bae-connect-cards.js";
-import {make_link_from_fileName, upload_file} from "./bae-connect-files.js";
+import {delete_uploaded_file, make_link_from_fileName, upload_file} from "./bae-connect-files.js";
 import {alertTypes, appendNotification} from "./error-ui.js";
 
 const domIdOfCardDataForm = 'new-card-form';
@@ -17,7 +17,6 @@ function boxIdFromParams() {
 async function readBoxAndQuestionData() {
     // get query-parameters from url
     const boxId = boxIdFromParams();
-    console.log(boxId)
 
     let box = await read_box(boxId)
         .then((box) => {
@@ -31,8 +30,6 @@ async function readBoxAndQuestionData() {
         })
 
     // does this work?
-    console.log(box);
-    console.log(box.cardIds)
     let cards = box.cardIds.map(async (cardId) => {
         let cardRead = await read_card(cardId)
         return cardRead;
@@ -41,6 +38,13 @@ async function readBoxAndQuestionData() {
     renderCards(cards);
 }
 
+function clearSecretStash() {
+    delete window[secretStashName];
+}
+
+/**
+ * entry point after document.ready
+ */
 $(document).ready(async function() {
 
     await readBoxAndQuestionData();
@@ -49,7 +53,11 @@ $(document).ready(async function() {
     newCardForm.addEventListener('submit', async event => {
         // event handler function for pressing new card button
         event.preventDefault()
-        event.stopPropagation()
+        event.stopPropagation();
+        // general handler nice for pressing enter. but this one button should not trigger change
+        if (event.submitter?.id === 'cancel-edit-button') {
+            return;
+        }
 
         const formData = new FormData(newCardForm);
 
@@ -79,19 +87,50 @@ $(document).ready(async function() {
         if (window[secretStashName]) {
             // secret stash should contain the card-id
             let assumedCardId = window[secretStashName]
-            console.log(`secretStash found: ${assumedCardId}`)
+            let mediaNameToDeleteOrUndefined;
+
+            // if media to clear keep the name in a local var for after-change-deletion
+            if ($('#clear-image-checkbox').is(":checked")) {
+                mediaNameToDeleteOrUndefined = (await read_card(assumedCardId)).media
+                cardToCreate.media = ''; // null does not work for the backend, so complete clear not possible it seems.
+            } else {
+                // check if there is previous media that needs deletion
+                if (cardToCreate.media) { // this being set means: code above uploaded new media and set the property
+                    // in that case get the current/previous value - we have not changed the card yet at this point
+                    mediaNameToDeleteOrUndefined = (await read_card(assumedCardId)).media
+                }
+            }
+
             await change_card(assumedCardId, cardToCreate);
-            delete window[secretStashName];
+
+            // only delete old media after change successful.
+            if (mediaNameToDeleteOrUndefined) {
+                // falsy includes undefined, null, other things, and also the empty string so the if can look like this
+                // https://developer.mozilla.org/en-US/docs/Glossary/Falsy
+                // empty string important because (reasons) that might be the value indicating "none".
+                await delete_uploaded_file(mediaNameToDeleteOrUndefined)
+            }
+
+            clearSecretStash();
             setUIToCreate();
         } else {
-            await create_card(cardToCreate)
+            await create_card(cardToCreate);
         }
         resetForm()
         await readBoxAndQuestionData();
 
     });
 
+    cancelEditButton = $(`<button id="cancel-edit-button" class="btn btn-primary">Bearbeiten abbrechen</button>`)
+    cancelEditButton.hide()
+    $(newCardForm).append(cancelEditButton);
+    cancelEditButton.click(event => {
+        cancelEditButton.hide();
+        setUIToCreate();
+        clearSecretStash();
+    })
 });
+let cancelEditButton;
 
 function renderFormNewCard(data){
     $('#box-title').empty().append(data.title);
@@ -107,32 +146,43 @@ function setTitleOfSubForm(title) {
 function setTitleOfMainButton(title) {
     document.getElementById(domIdOfMainButton).innerText = title;
 }
-function setTitleToEdit() {
-    setTitleOfSubForm("Frage bearbeiten");
-}
-function setTitleToCreate() {
-    setTitleOfSubForm("Neue Frage:");
-}
-function setButtonToEdit() {
-    setTitleOfMainButton('Frage ändern')
-}
-function setButtonToCreate() {
-    setTitleOfMainButton('Frage erstellen')
-}
+
 function setUIToEdit() {
-    setButtonToEdit();
-    setTitleToEdit();
+    setTitleOfMainButton('Frage ändern')
+    setTitleOfSubForm("Frage bearbeiten");
+    cancelEditButton.show();
 }
 function setUIToCreate() {
-    setButtonToCreate();
-    setTitleToCreate();
+    resetForm()
+    setTitleOfMainButton('Frage erstellen')
+    setTitleOfSubForm("Neue Frage:");
+    clearCardImagePreview();
+    cancelEditButton.hide()
 }
+
+function clearCardImagePreview() {
+    $('#currentImagePreview').empty();
+}
+
+function setCardPreviewImage(card) {
+    clearCardImagePreview()
+    if (card.media) {
+        $('#currentImagePreview')
+            .append(
+                $(`<img src="${make_link_from_fileName(card.media)}" style="max-width: 3em; max-height: 3em;" class="mb-2">`)
+            )
+            .append(
+                $(`<input type="checkbox" name="clear-image-checkbox" id="clear-image-checkbox" class="m-2">Bild entfernen</input>`)
+            )
+    }
+    ;
+}
+
 
 function renderCards(cards){
     $("#cards").empty();
     $.each(cards, async function (i, cardPromise) {
         let card = await cardPromise;
-        console.log(card);
         // todo fragen per box filter testen bei erfolgreicher abfrage der aktuellen kartei aus dem backend
         // todo fragen element fertig machen
         const $cardElement = $("<div>").addClass("list-group-item list-group-item-action d-flex justify-content-between align-items-center");
@@ -163,13 +213,7 @@ function renderCards(cards){
             setUIToEdit();
             document.getElementById('question').value = card.question;
             document.getElementById('answer').value = card.answer;
-            $('#currentImagePreview').empty();
-            if (card.media) {
-                $('#currentImagePreview')
-                    .append(
-                        $(`<img src="${make_link_from_fileName(card.media)}" style="max-width: 3em; max-height: 3em;" class="mb-2">`)
-                    )
-            };
+            setCardPreviewImage(card);
         })
 
         $cardElement.append($buttonGroup);
