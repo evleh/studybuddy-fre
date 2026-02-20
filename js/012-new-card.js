169d2a594f960e7e5a41f8aaae70f05b3b1ed274
ww24e018 <ww24e018@technikum-wien.at>
2026-02-20 09:48:30 +0100

    Feature/73 new cards (#75)
    * delete dummy data
    
    * remove link from new card button
    
    * make code more working that not.
    
    * save temp state
    
    * make and fix edit things
    
    * code review

--- js/012-new-card.js
+++ js/012-new-card.js
@@ -1,18 +1,26 @@
-import constants from "./constants.js";
 import {read_box} from "./bae-connect-boxes.js";
-import {read_card} from "./bae-connect-cards.js";
+import {change_card, create_card, delete_card, read_card} from "./bae-connect-cards.js";
+import {make_link_from_fileName, upload_file} from "./bae-connect-files.js";
+import {alertTypes, appendNotification} from "./error-ui.js";
 
-let boxTitle = "Bezirke Wien"
+const domIdOfCardDataForm = 'new-card-form';
+const domIdOfMainButton = 'new-card-button';
+const secretStashName = 'stateStashCardEditId'
+
+function boxIdFromParams() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get("id")
+}
+
+
 
 async function readBoxAndQuestionData() {
     // get query-parameters from url
-    const params = new URLSearchParams(window.location.search);
-    const boxId = params.get("id")
+    const boxId = boxIdFromParams();
     console.log(boxId)
 
     let box = await read_box(boxId)
         .then((box) => {
-            boxTitle = box.title
             renderFormNewCard(box)
             return Promise.resolve(box); // to be able to wait at the read_box level.
         })
@@ -36,21 +44,107 @@ async function readBoxAndQuestionData() {
 $(document).ready(async function() {
 
     await readBoxAndQuestionData();
-    // todo: register handler on button "neue frage erstellen" und make that work
+
+    const newCardForm = document.getElementById(domIdOfCardDataForm);
+    newCardForm.addEventListener('submit', async event => {
+        // event handler function for pressing new card button
+        event.preventDefault()
+        event.stopPropagation()
+
+        const formData = new FormData(newCardForm);
+
+        let boxId = boxIdFromParams()
+        let cardToCreate = {
+            'boxId': boxId,
+            'question': formData.get('question'),
+            'answer': formData.get('answer')
+        };
+
+        // TODO media upload and clearing of form
+        let imageField = formData.get('imageInput');
+        if (imageField.name !== '') {
+            cardToCreate.media = await upload_file({
+                filename: imageField.name,
+                fileOrBlob: imageField
+            }).catch(() => appendNotification({
+                message:'Upload fehlgeschlagen',
+                type: alertTypes.ERROR
+            }));
+            // clear after upload
+            const fileInput = document.getElementById("imageInput");
+            fileInput.value = '';
+        }
+
+        // if secret-stash exists do an update clear stash and UI state
+        if (window[secretStashName]) {
+            // secret stash should contain the card-id
+            let assumedCardId = window[secretStashName]
+            console.log(`secretStash found: ${assumedCardId}`)
+            await change_card(assumedCardId, cardToCreate);
+            delete window[secretStashName];
+            setUIToCreate();
+        } else {
+            await create_card(cardToCreate)
+        }
+        resetForm()
+        await readBoxAndQuestionData();
+
+    });
 
 });
 
 function renderFormNewCard(data){
-    $('#box-title').append(data.title);
+    $('#box-title').empty().append(data.title);
+}
+
+
+function resetForm() {
+    document.getElementById(domIdOfCardDataForm).reset();
+}
+function setTitleOfSubForm(title) {
+    document.getElementById('card-data-form-h3-tag').innerText = title;
+}
+function setTitleOfMainButton(title) {
+    document.getElementById(domIdOfMainButton).innerText = title;
+}
+function setTitleToEdit() {
+    setTitleOfSubForm("Frage bearbeiten");
+}
+function setTitleToCreate() {
+    setTitleOfSubForm("Neue Frage:");
+}
+function setButtonToEdit() {
+    setTitleOfMainButton('Frage ändern')
+}
+function setButtonToCreate() {
+    setTitleOfMainButton('Frage erstellen')
+}
+function setUIToEdit() {
+    setButtonToEdit();
+    setTitleToEdit();
+}
+function setUIToCreate() {
+    setButtonToCreate();
+    setTitleToCreate();
 }
 
 function renderCards(cards){
-    $.each(cards, function(i, card){
+    $("#cards").empty();
+    $.each(cards, async function (i, cardPromise) {
+        let card = await cardPromise;
+        console.log(card);
         // todo fragen per box filter testen bei erfolgreicher abfrage der aktuellen kartei aus dem backend
         // todo fragen element fertig machen
         const $cardElement = $("<div>").addClass("list-group-item list-group-item-action d-flex justify-content-between align-items-center");
 
-        const $title = $("<span>").text("Frage: " + card.question);
+        const $title = $(`<span title="${card.answer}">`).text("Frage: " + card.question);
+        $cardElement.append($title);
+
+        if (card.media) {
+            const $image = $(`<img src="${make_link_from_fileName(card.media)}" style="max-width: 3em; max-height: 3em;" class="mb-2">`);
+            $cardElement.append($image);
+        }
+
 
         const $editBtn = $("<button>").text("Bearbeiten").addClass("btn btn-sm btn-outline-primary")
         const $deleteBtn = $("<button>").text("Löschen").addClass("btn btn-sm btn-outline-primary")
@@ -59,7 +153,26 @@ function renderCards(cards){
             .append($editBtn)
             .append($deleteBtn);
 
-        $cardElement.append($title).append($buttonGroup);
+        $deleteBtn.click(async event => {
+            await delete_card(card.id)
+            await readBoxAndQuestionData();
+        });
+        $editBtn.click(async event => {
+            window[secretStashName] = card.id;
+            resetForm(); // clear the image if set
+            setUIToEdit();
+            document.getElementById('question').value = card.question;
+            document.getElementById('answer').value = card.answer;
+            $('#currentImagePreview').empty();
+            if (card.media) {
+                $('#currentImagePreview')
+                    .append(
+                        $(`<img src="${make_link_from_fileName(card.media)}" style="max-width: 3em; max-height: 3em;" class="mb-2">`)
+                    )
+            };
+        })
+
+        $cardElement.append($buttonGroup);
         $("#cards").append($cardElement);
 
     })
